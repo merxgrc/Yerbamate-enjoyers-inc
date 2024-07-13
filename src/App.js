b67b0a6353c0b5bc9a8c1ee192588b2402201024
merxgrc <yeetgawdmcneckass@gmail.com>
2024-07-13 02:08:28 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
-import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SignUp />} />
-        
       </Routes>
     </Router>
   );
