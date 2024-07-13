0e57b1ca9d080028821ab5d93c78dd2b6f8edb18
merxgrc <yeetgawdmcneckass@gmail.com>
2024-07-13 01:22:08 -0700

    created a Sign up page and got everything set up with css and reactjs

---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg rounded-lg">
+        <h3 className="text-2xl font-bold text-center text-primary">Log In</h3>
+        <form action="">
+          <div className="mt-4">
+            <div>
+              <label className="block" htmlFor="email">Email</label>
+              <input type="email" placeholder="Email"
+                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
+            </div>
+            <div className="mt-4">
+              <label className="block">Password</label>
+              <input type="password" placeholder="Password"
+                className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-primary" />
+            </div>
+            <div className="flex items-baseline justify-between">
+              <button className="px-6 py-2 mt-4 text-white bg-primary rounded-lg hover:bg-blue-900">Log In</button>
+              <a href="#" className="text-sm text-primary hover:underline">Forgot password?</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
